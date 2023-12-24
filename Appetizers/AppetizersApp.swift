649460f34144449689c7d2d255bd80fbe9df680a
Davron Abdukhakimov <davron0506150@gmail.com>
2023-12-24 14:14:08 +0500

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Davron Abdukhakimov on 24/12/23.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
